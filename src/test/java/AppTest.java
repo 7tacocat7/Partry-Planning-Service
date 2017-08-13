cf8b56414178fed11ee72f5e30c9eda942dd3ac5
Carson Canales <cc020889@icloud.com>
2017-08-12 21:40:16 -0700

    created second test

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -12,5 +12,9 @@ public class AppTest {
         Event testEvent = new Event("Carsons birthday",50,"thai food","open bar","live band", "");
         assertEquals(true, testEvent instanceof  Event);
     }
-
+    @Test
+    public void newEvent_getsName_carson(){
+        Event testEvent = new Event("phill", 100,"mexican food","wine","dj", "grill");
+        assertEquals("carson",testEvent.getName());
+    }
 }
